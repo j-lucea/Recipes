69d926c0bb0050b26a94b287c6524951aa9035ab
jlucea <jonathan.lucea@gmail.com>
2024-06-04 22:32:39 +0200

    Twig done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,6 @@ class HomeController extends AbstractController
 {
     #[Route("/", name: "home")]
     function index(Request $request): Response {
-        return new Response('Bonjour ' . $request->query->get('name', 'Inconnu'));
+        return $this->render('home/index.html.twig');
     }
 }
--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -9,14 +9,17 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recette', name: 'recipe.index')]
+    #[Route('/recettes', name: 'recipe.index')]
     public function index(Request $request): Response
     {
-        return new Response('Recettes');
+        return $this->render('recipe/index.html.twig');
     }
-    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
     public function show(Request $request, string $slug, int $id): Response
     {
-        return new Response('Recette : ' . $slug);
+        return $this->render('recipe/show.html.twig', [
+            'slug' => $slug,
+            'id' => $id
+        ]);
     }
 }
